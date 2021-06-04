65ab4ac484b27624ecd5fae047a5b3a78dc2f8c0
Bryan Gonzalez <bgonza868@gmail.com>
2021-06-03 21:21:58 -0400

    fix(deps): upgrade everything that depends babel

--- src/ListeningPostWebUi/src/main.js
+++ src/ListeningPostWebUi/src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App, { mountPoint } from './App.vue'
