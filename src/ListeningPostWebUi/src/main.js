71aa5f392716e5807935b323a148505559344dcc
Bryan Gonzalez <bgonza868@gmail.com>
2021-06-04 21:35:52 -0400

    feat(app): upgrade to webpack 5

--- src/ListeningPostWebUi/src/main.js
+++ src/ListeningPostWebUi/src/main.js
@@ -1,12 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import App, { mountPoint } from './App.vue'
+import App from './App.vue'
 import router from './router'
-// import CKEditor from '@ckeditor/ckeditor5-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -16,15 +13,9 @@ Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-// Vue.use(LayoutPlugin)
-// Vue.use(CardPlugin)
-// Vue.use(VBScrollspyPlugin)
-// Vue.use(CKEditor)
 
 /* eslint-disable no-new */
 new Vue({
-  el: mountPoint,
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: (h) => h(App)
+}).$mount('#app')
